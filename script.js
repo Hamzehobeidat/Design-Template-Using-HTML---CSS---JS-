f05b47b67c8c9523a0b36f05d65cca7123e86322
Hamzehobidat <hamzehob94@gmail.com>
2019-11-24 15:10:47 +0200

    Create Project Design Template Using ( HTML - CSS - JS )

---
+++ script.js
@@ -0,0 +1,25 @@
+function HoverButton()
+{
+    document.getElementById("btn").style.backgroundColor="#080";
+    document.getElementById("btn").style.cursor="pointer";
+}
+
+function hover()
+{
+    document.getElementById("btn").style.backgroundColor="#00BC8A";
+}
+
+function hiden(){
+    
+    document.getElementById("vis").style.visibility="visible";
+}
+
+function visiple()
+{
+    document.getElementById("vis").style.visibility="hidden";
+}
+
+function color()
+{
+    document.getElementById("twcolor").style.color="red";
+}
